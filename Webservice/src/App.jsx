b80d85bdadb5fb6507a3f8b9aac9df7dad43f2ec
Huge <h.clement@condorcet.be>
2024-03-21 19:40:29 +0100

    Exos 2.5 fini

--- Webservice/src/App.jsx
+++ Webservice/src/App.jsx
@@ -1,11 +1,31 @@
 import './App.css'
 import Gallery from './Components/Gallery.jsx'
 import pics from './data.jsx'
+import Search from './Components/Search.jsx'
+import {useState,useEffect} from 'react'
+
+
+
 
 function App() { // This is a functional component
+
+  const [search, setSearch] = useState('');
+  const [filteredPics, setFilteredPics] = useState(pics);
+
+  useEffect(() => {
+    const filtered = search ? pics.filter(pic => pic.author.toLowerCase().includes(search.toLowerCase())) : pics;
+    setFilteredPics(filtered);
+
+  }, [search])
+
+
+
+
+
   return (
     <div id='appH'>
-      <Gallery pics={pics} />
+      <Search onSearch={setSearch} />
+      <Gallery pics={filteredPics} />
     </div> 
   )
 }
---
+++ Webservice/src/Components/Search.jsx
@@ -0,0 +1,10 @@
+const Search = (props) =>{
+    return(
+        <div>
+            <label htmlFor="search">Search by author</label>
+            <input type="text" id="search"  placeholder="Search..." onChange={(e) => props.onSearch(e.target.value)}/>
+        </div>
+    )
+}
+
+export default Search; // This is a functional component
